a41e2719b945f2bfa0f7d3b9258bb83e193ad1a6
Juan Manuel <manuelsima0305@gmail.com>
2024-10-28 20:26:52 -0600

    El Rey de React

--- frontend/src/pages/AdminProduct.jsx
+++ frontend/src/pages/AdminProduct.jsx
@@ -31,7 +31,7 @@ const Productos = () => {
   // Filtrar productos según el término de búsqueda
   const filteredProducts = productos.filter(product =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    product.client.toLowerCase().includes(searchTerm.toLowerCase())
+    (product.seller_id?.name?.toLowerCase().includes(searchTerm.toLowerCase()) || "")
   );
 
   // Calcular paginación
@@ -53,8 +53,8 @@ const Productos = () => {
         
         <div className="product-summary">
           <span>Total de Productos: {productos.length}</span>
-          <span>Productos Activos: {productos.filter(product => product.active).length}</span>
-          <span>Productos Inactivos: {productos.filter(product => !product.active).length}</span>
+          <span>Productos Activos: {productos.filter(product => product.isActive).length}</span>
+          <span>Productos Inactivos: {productos.filter(product => !product.isActive).length}</span>
         </div>
         
         <div className="actions">
@@ -91,21 +91,21 @@ const Productos = () => {
               </tr>
             </thead>
             <tbody>
-              {currentProducts.map((product, index) => (
-                <tr key={index}>
-                  <td>{indexOfFirstProduct + index + 1}</td>
-                  <td>{product.name}</td>
-                  <td>{product.category}</td>
-                  <td>{product.client}</td>
-                  <td>${product.price}</td>
-                  <td>{product.quantity}</td>
-                  <td>{product.active ? "Activo" : "Inactivo"}</td>
-                  <td>
-                    <button className="action-button view-button">Ver Detalles</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
+  {currentProducts.map((product, index) => (
+    <tr key={index}>
+      <td>{indexOfFirstProduct + index + 1}</td>
+      <td>{product.name}</td>
+      <td>{product.category}</td>
+      <td>{product.seller_id?.name || "Sin información"}</td>
+      <td>${product.price}</td>
+      <td>{product.stock || 0}</td>
+      <td>{product.isActive ? "Activo" : "Inactivo"}</td>
+      <td>
+        <button className="action-button view-button">Ver Detalles</button>
+      </td>
+    </tr>
+  ))}
+</tbody>
           </table>
         )}
         
