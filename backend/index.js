657701939682a0b5e51f05f56d5fe7f20b665813
Juan Manuel <manuelsima0305@gmail.com>
2024-11-05 04:57:15 -0600

    hay sueñito son las 4:57 AM

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configurar CORS
 app.use(cors({
-    origin: 'https://marketapp-backend.onrender.com', // Cambia esto por el origen de tu frontend
+    origin: 'https://marketapp-frontend.vercel.app/', // Cambia esto por el origen de tu frontend
     credentials: true,
 }));
 
