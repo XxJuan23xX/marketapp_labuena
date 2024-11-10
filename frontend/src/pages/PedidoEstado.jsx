d24c975f530b7178c5669c5cadafc3d854e0eedb
Juan Manuel <manuelsima0305@gmail.com>
2024-11-10 17:54:18 -0600

    EL BUEN DERRAPES

--- frontend/src/pages/PedidoEstado.jsx
+++ frontend/src/pages/PedidoEstado.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import api from '../../api';
-import './PedidoEstado.css';
 import Navbar from '../components/navbar/navbarComponent';
+import './PedidoEstado.css';
 
 const PedidoEstado = () => {
   const { orderId } = useParams(); // Obtenemos orderId desde la URL
@@ -15,7 +15,7 @@ const PedidoEstado = () => {
         const response = await api.get(`/api/orders/${orderId}`);
         console.log("Datos de la orden:", response.data); // Verifica que datos recibes aquí
         setOrder(response.data);
-    
+
         if (response.data.status === 'completado') {
           setOrderStatus(3);
         }
@@ -54,49 +54,52 @@ const PedidoEstado = () => {
   if (!order) return <p>Cargando detalles de la orden...</p>;
 
   return (
-    <div className="pedido-estado-page">
-      <Navbar></Navbar>
-      {/* Breadcrumb */}
-      <div className="breadcrumb">
-        <Link to="/products" className="breadcrumb-link">← Volver a Mis Productos</Link>
-      </div>
-
-      <div className="pedido-estado-card">
-        <h1>Detalles del Pedido</h1>
-        <div className="phase-indicator">
-          <div className="phase completed">
-            <div className="phase-circle completed">1</div>
-            Venta Iniciada
-          </div>
-          <div className={`phase ${orderStatus >= 2 ? 'completed' : ''}`}>
-            <div className={`phase-circle ${orderStatus >= 2 ? 'completed' : ''}`}>2</div>
-            Venta en Proceso
-          </div>
-          <div className={`phase ${orderStatus === 3 ? 'completed' : ''}`}>
-            <div className={`phase-circle ${orderStatus === 3 ? 'completed' : ''}`}>3</div>
-            Venta Finalizada
-          </div>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: getProgressWidth() }}></div>
-          </div>
+    <>
+      <Navbar />
+      <div className="pedido-estado-page">
+        
+        {/* Breadcrumb */}
+        <div className="breadcrumb">
+          <Link to="/products" className="breadcrumb-link">← Volver a Mis Productos</Link>
         </div>
 
-        <div className="product-info">
-          <p><strong>Precio:</strong> ${order.price}</p>
-          <p><strong>Comprador:</strong> {order.buyer_id?.name || "Nombre del comprador no disponible"}</p>
+        <div className="pedido-estado-card">
+          <h1>Detalles del Pedido</h1>
+          <div className="phase-indicator">
+            <div className="phase completed">
+              <div className="phase-circle completed">1</div>
+              Venta Iniciada
+            </div>
+            <div className={`phase ${orderStatus >= 2 ? 'completed' : ''}`}>
+              <div className={`phase-circle ${orderStatus >= 2 ? 'completed' : ''}`}>2</div>
+              Venta en Proceso
+            </div>
+            <div className={`phase ${orderStatus === 3 ? 'completed' : ''}`}>
+              <div className={`phase-circle ${orderStatus === 3 ? 'completed' : ''}`}>3</div>
+              Venta Finalizada
+            </div>
+            <div className="progress-bar">
+              <div className="progress" style={{ width: getProgressWidth() }}></div>
+            </div>
+          </div>
+
+          <div className="product-info">
+            <p><strong>Precio:</strong> ${order.price}</p>
+            <p><strong>Comprador:</strong> {order.buyer_id?.name || "Nombre del comprador no disponible"}</p>
+          </div>
+
+          {orderStatus < 3 && (
+            <button className="finalize-sale-button" onClick={handleFinalizeSale}>
+              Confirmar y Finalizar Venta
+            </button>
+          )}
+
+          {orderStatus === 3 && (
+            <p className="completed-message">La venta ha sido completada exitosamente.</p>
+          )}
         </div>
-
-        {orderStatus < 3 && (
-          <button className="finalize-sale-button" onClick={handleFinalizeSale}>
-            Confirmar y Finalizar Venta
-          </button>
-        )}
-
-        {orderStatus === 3 && (
-          <p className="completed-message">La venta ha sido completada exitosamente.</p>
-        )}
       </div>
-    </div>
+    </>
   );
 };
 
