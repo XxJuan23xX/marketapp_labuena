b8337920f65e0138926bfac376dbaac1f30431f4
Juan Manuel <manuelsima0305@gmail.com>
2024-11-28 22:41:33 -0600

    Subastas flash

--- backend/routes/auctionRoutes.js
+++ backend/routes/auctionRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const auctionController = require('../controllers/auctionController');
 
+router.get('/flash-auctions', auctionController.getFlashAuctions);
 router.post('/', auctionController.createAuction);
 router.get('/', auctionController.getAuctions);
 router.get('/:id', auctionController.getAuctionById);
